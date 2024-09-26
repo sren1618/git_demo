9dea8f412468e02ad4f1a2cd81466600f9e740b4
sren <sren16@outlook.com>
2024-09-26 20:20:59 +1000

    feat: add contact us page

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res) {
     res.send('Hello World')
 })
 
+app.get('/contact', function(req, res) {
+    res.send('Contact Us')
+})
+
 app.listen(8080, function() {
     console.log('Listening at port 8080.')
 })
